9346eb55e835dd461087b9863a5dd251fdc812c6
Evanl202 <evanlin202@gmail.com>
2025-10-27 13:49:01 -0400

    Update MLFQ.c

--- MLFQ.c
+++ MLFQ.c
@@ -1,3 +1,99 @@
 #include <stdio.h>
-#include "process.h"
+#include <stdlib.h>
+#include "queue.h"
 #include "utils.h"
+
+#define Q2 4
+#define Q1 8
+
+void mlfq(Process processes[], int n) {
+  Node *q2_head = NULL, *q2_tail = NULL;
+  Node *q1_head = NULL, *q1_tail = NULL;
+  Node *q0_head = NULL, *q0_tail = NULL;
+
+  int time = 0, completed = 0;
+  int gantt[200], time_line[200];
+  int count = 0;
+  
+  for (int i = 0; i < n; i++) {
+    processes[i].remaining_time = processes[i].burst_time;
+    processes[i].completed = 0;
+  }
+  for (int i = 0; i < n; i++) {
+      if (processes[i].arrival_time == 0) 
+          enqueue(&q2_head, &q2_tail, &processes[i]);
+  }
+  while (completed < n) {
+      Process *p = NULL;
+      int current_quantum = 0;
+      Node **current_head = NULL, **current_tail = NULL;
+
+      if (q2_head) {
+          p = dequeue(&q2_head, &q2_tail);
+          current_quantum = Q2;
+          current_head = &q2_head;
+          current_tail = &q2_tail;
+      } else if (q1_head) {
+          p = dequeue(&q1_head, &q1_tail);
+          current_quantum = Q1;
+          current_head = &q1_head;
+          current_tail = &q1_tail;
+      } else if (q0_head) {
+          p = dequeue(&q0_head, &q0_tail);
+          current_quantum = p->remaining_time;
+          current_head = &q0_head;
+          current_tail = &q0_tail;
+      }
+      if (!p) {
+        time++;
+        for (int i = 0; i < n; i++) {
+          if (processes[i].arrival_time <= time &&
+              !processes[i].completed &&
+              processes[i].remaining_time == processes[i].burst_time) {
+              enqueue(&q2_head, &q2_tail, &processes[i]);
+          }  
+        }
+        continue;
+      }
+    
+      if (time < p->arrival_time)
+        time = p->arrival_time;
+      
+      int exec_time = (p->remaining_time > current_quantum) ? current_quantum : p->remaining_time;
+      
+      gantt[count] = p->pid;
+      time_line[count] = time;
+      count++;
+
+      time += exec_time;
+      p->remaining_time -= exec_time;
+
+      for (int i = 0; i < n; i++) {
+        if(processes[i].arrival_time <= time &&
+          !processes[i].completed &&
+          processes[i].remaining_time == processes[i].burst_time &&
+          &processes[i] != p) {
+          enqueue(&q2_head, &q2_tail, &processes[i]);
+        }
+      }
+      
+      if (p->remaining_time == 0) {
+        p-> completed = 1;
+        p->turnaround_time = time - p->arrival_time;
+        p->waiting_time = p->turnaround_time - p->burst_time;
+
+        completed++;
+      } else {
+          if(current_head == &q2_head)
+            enqueue(&q1_head, &q1_tail, p);
+          else if (current_head == &q1_head)
+            enqueue(&q0_head, &q0_tail, p);
+          else
+            enqueue(&q0_head, &q0_tail, p);
+      }
+  }
+  time_line[count] = time;
+
+  printGantt(gantt, time_line, count);
+  printResults(processes, n);
+}
