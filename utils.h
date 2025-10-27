8345f7a1ae17bf16b6e90301b7c8f5ce22129462
Evanl202 <evanlin202@gmail.com>
2025-10-27 13:04:54 -0400

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,1 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
 
+#include "process.h"
+
+// Prints a Gantt chart
+void printGantt(int gantt[], int time_line[], int count);
+
+// Prints waiting time, turnaround time, and averages
+void printResults(Process processes[], int n);
+
+#endif
