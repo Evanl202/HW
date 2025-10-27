403c05591af248215037fda00b112e61b7c49e7f
Evanl202 <evanlin202@gmail.com>
2025-10-27 12:54:47 -0400

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,1 +1,15 @@
+#ifndef QUEUE_H
+#define QUEUE_H
 
+#include "process.h"
+//Class for Node and functions
+typedef struct Node {
+  Process *process;
+  struct Node *next;
+} Node;
+
+//Queue functions
+void enqueue(Node **head, Node **tail, Process *p);
+Process* dequeue(Node **head, Node **tail);
+
+#endif
