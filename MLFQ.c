81e2d4497ea7da7b450e3a8f63f2e1cf0ac33f24
Evanl202 <evanlin202@gmail.com>
2025-10-27 14:42:18 -0400

    Update MLFQ.c

--- MLFQ.c
+++ MLFQ.c
@@ -18,7 +18,10 @@ void mlfq(Process processes[], int n) {
   int finished_queue[n];
   int pid_index_map[1000];
   
-  for (int i = 0; i < 1000; i++) pid_index_map[i] = -1;
+  for (int i = 0; i < n; i++) {
+    pid_index_map[processes[i].pid] = i; 
+}
+
   for (int i = 0; i < n; i++) {
     processes[i].remaining_time = processes[i].burst_time;
     processes[i].completed = 0;
@@ -84,23 +87,23 @@ void mlfq(Process processes[], int n) {
       }
       
       if (p->remaining_time == 0) {
-        p-> completed = 1;
-        p->turnaround_time = time - p->arrival_time;
-        p->waiting_time = p->turnaround_time - p->burst_time;
-        completed++;
-
-        int idx = pid_index_map[p->pid];
-        if (current_head == &q2_head && p->remaining_time == 0) finished_queue[idx] = 1;
-        else if (current_head == &q1_head && p->remaining_time == 0) finished_queue[idx] = 2;
-        else finished_queue[idx] = 3;
+          p->completed = 1;
+          p->turnaround_time = time - p->arrival_time;
+          p->waiting_time = p->turnaround_time - p->burst_time;
+          completed++;
+      
+          int idx = pid_index_map[p->pid]; // map PID to index
+          // Assign finished queue based on the quantum used
+          if (current_quantum == Q2) finished_queue[idx] = 1;
+          else if (current_quantum == Q1) finished_queue[idx] = 2;
+          else finished_queue[idx] = 3;
       } else {
-          if(current_head == &q2_head)
-            enqueue(&q1_head, &q1_tail, p);
-          else if (current_head == &q1_head)
-            enqueue(&q0_head, &q0_tail, p);
-          else
-            enqueue(&q0_head, &q0_tail, p);
+          // Demote unfinished processes
+          if (current_head == &q2_head) enqueue(&q1_head, &q1_tail, p);
+          else if (current_head == &q1_head) enqueue(&q0_head, &q0_tail, p);
+          else enqueue(&q0_head, &q0_tail, p);
       }
+
   }
   time_line[count] = time;
 
